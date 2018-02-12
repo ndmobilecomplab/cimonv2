8dd9596311f12e452b2c00253abcdab9d9b4adfe
uttam028 <afzalhossain@afzals-mbp.dhcp.nd.edu>
2018-02-11 22:38:53 -0500

    First Commit

---
+++ cimonv2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cimonv2
+//
+//  Created by Afzal Hossain on 2/11/18.
+//  Copyright © 2018 Afzal Hossain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
