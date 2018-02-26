c2982d35b4538b001c1600db7ee6248f556d06b6
John Templeton <jtemplet@johns-air.dhcp.nd.edu>
2018-02-26 16:24:28 -0500

    added target task for implementation

--- cimonv2/CommonUtils/DummyData.swift
+++ cimonv2/CommonUtils/DummyData.swift
@@ -10,7 +10,7 @@ import Foundation
 
 
 class DummyData: NSObject {
-
+    
     static func getDummySurveys()->[Survey]{
         
         //signature
@@ -22,31 +22,61 @@ class DummyData: NSObject {
         //trace shapes with speech
         let task3 = Task(id: 3, text: "", type: "MT003", possibleInput: "", orderId: 3, isActive: 1, isRequired: 0, parentTaskId: -1, hasChild: 0, inputToTriggerChild: "", defaultInput: "")
         
-        //motor function
-        let task4 = Task(id: 2, text: "", type: "MT005", possibleInput: "", orderId: 2, isActive: 1, isRequired: 0, parentTaskId: -1, hasChild: 0, inputToTriggerChild: "", defaultInput: "")
+        //trace shapes with cognitive load
+        let task4 = Task(id: 4, text: "", type: "MT004", possibleInput: "", orderId: 4, isActive: 1, isRequired: 0, parentTaskId: -1, hasChild: 0, inputToTriggerChild: "", defaultInput: "")
+        
+        //gross trace shapes
+        let task5 = Task(id: 5, text: "", type: "MT005", possibleInput: "", orderId: 5, isActive: 1, isRequired: 0, parentTaskId: -1, hasChild: 0, inputToTriggerChild: "", defaultInput: "")
+        
+        //gross trace shapes with speech
+        let task6 = Task(id: 6, text: "", type: "MT006", possibleInput: "", orderId: 6, isActive: 1, isRequired: 0, parentTaskId: -1, hasChild: 0, inputToTriggerChild: "", defaultInput: "")
+        
+        //gross trace shapes with cognitive load
+        let task7 = Task(id: 7, text: "", type: "MT007", possibleInput: "", orderId: 7, isActive: 1, isRequired: 0, parentTaskId: -1, hasChild: 0, inputToTriggerChild: "", defaultInput: "")
         
         //memory game
-        let task5 = Task(id: 3, text: "", type: "MT008", possibleInput: "", orderId: 3, isActive: 1, isRequired: 0, parentTaskId: -1, hasChild: 0, inputToTriggerChild: "", defaultInput: "")
+        let task8 = Task(id: 8, text: "", type: "MT008", possibleInput: "", orderId: 8, isActive: 1, isRequired: 0, parentTaskId: -1, hasChild: 0, inputToTriggerChild: "", defaultInput: "")
         
+        //falling ball
+        let task9 = Task(id: 9, text: "", type: "MT009", possibleInput: "", orderId: 9, isActive: 1, isRequired: 0, parentTaskId: -1, hasChild: 0, inputToTriggerChild: "", defaultInput: "")
         
+        //target
+        let task10 = Task(id: 10, text: "", type: "MT010", possibleInput: "", orderId: 10, isActive: 1, isRequired: 0, parentTaskId: -1, hasChild: 0, inputToTriggerChild: "", defaultInput: "")
+        
+        //viospatial
+        let task11 = Task(id: 11, text: "", type: "MT011", possibleInput: "", orderId: 11, isActive: 1, isRequired: 0, parentTaskId: -1, hasChild: 0, inputToTriggerChild: "", defaultInput: "")
+        
+        //color
+        let task12 = Task(id: 12, text: "", type: "MT012", possibleInput: "", orderId: 12, isActive: 1, isRequired: 0, parentTaskId: -1, hasChild: 0, inputToTriggerChild: "", defaultInput: "")
+        
+        //picture
+        let task13 = Task(id: 13, text: "", type: "MT013", possibleInput: "", orderId: 13, isActive: 1, isRequired: 0, parentTaskId: -1, hasChild: 0, inputToTriggerChild: "", defaultInput: "")
+        
+        //balance
+        let task14 = Task(id: 14, text: "", type: "MT014", possibleInput: "", orderId: 14, isActive: 1, isRequired: 0, parentTaskId: -1, hasChild: 0, inputToTriggerChild: "", defaultInput: "")
+        
+        //connect the dots
+        let task15 = Task(id: 15, text: "", type: "MT015", possibleInput: "", orderId: 15, isActive: 1, isRequired: 0, parentTaskId: -1, hasChild: 0, inputToTriggerChild: "", defaultInput: "")
         
         //survey 1
-        let survey1 = Survey(studyId: "p001", surveyId: "s001", versionId: 1, taskList: [task1, task2, task3])
+        let survey1 = Survey(studyId: "p001", surveyId: "s001", versionId: 1, taskList: [task10])
         
         //Survey 2
         let survey2 = Survey(studyId: "p001", surveyId: "s002", versionId: 1, taskList: [task1, task4, task5])
         
-        
         //case 1: one survey, show survey 1
-        //return [survey1]
+        return [survey1]
         
         //case 2: one survey, show survey 2
         //return [survey2]
         
         //case 3: two surveys, show both
-        return [survey1, survey2]
+        //return [survey1, survey2]
     }
-
+    
 }
+
+
+
 
 
---
+++ cimonv2/TaskManagement/TargetTask/TargetGameViewController.swift
@@ -0,0 +1,131 @@
+//
+//  TargetGameViewController.swift
+//  MultiTestsApp
+//
+//  Created by Collin Klenke on 2/6/18.
+//  Copyright © 2018 NDMobileCompLab. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TargetGameViewController: UIViewController {
+    
+    @IBOutlet weak var countDownLabel: UILabel!
+    
+    var countdown = Timer()
+    var countDownCount: Int = 30
+    var lastPos: Int = 6
+    
+    var largeCircle = UIButton()
+    var smallCircle = UIButton()
+    
+    var playing: Bool = false
+    var tapCounter: Int = 0
+    var largeCircleTapCounter: Int = 0
+    var smallCircleTapCounter: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.countdown = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
+        self.playing = true
+        self.newTarget()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func newTarget() {
+        
+        let radius:CGFloat = 100
+        let iradius = Int(radius)
+        let posX = Int(UIScreen.main.bounds.width/2 - radius)
+        let posY = Int(UIScreen.main.bounds.height/2 - radius)
+        
+        var pos = Int(arc4random()%4)
+        while (pos == self.lastPos){
+            pos = Int(arc4random()%4)               //we want the position to change, so we keep track of the last position and put the next one somewhere else
+        }
+        self.lastPos = pos
+        
+        var x = Int(arc4random())%posX
+        var y = Int(arc4random())%posY
+        
+        switch pos{
+        case 0:
+            break
+        case 1:
+            x = x + Int(UIScreen.main.bounds.width/2)
+        case 2:
+            y = y + Int(UIScreen.main.bounds.height/2)
+        case 3:
+            x = x + Int(UIScreen.main.bounds.width/2)
+            y = y + Int(UIScreen.main.bounds.height/2)
+        default:
+            self.endGame()
+        }
+        
+        print("X: \(x) Y: \(y)")
+        
+        largeCircle.frame = CGRect(x: x, y: y, width: iradius, height: iradius)
+        largeCircle.layer.cornerRadius = radius/2
+        largeCircle.layer.backgroundColor = UIColor.black.cgColor
+        largeCircle.addTarget(self, action: #selector(largeCircleTapped), for: .touchDown)
+        
+        smallCircle.frame = CGRect(x: x + iradius/4, y: y + iradius/4, width: iradius/2, height: iradius/2)
+        smallCircle.layer.cornerRadius = radius/4
+        smallCircle.layer.backgroundColor = UIColor.red.cgColor
+        smallCircle.addTarget(self, action: #selector(smallCircleTapped), for: .touchDown)
+        
+        self.view.addSubview(largeCircle)
+        self.view.addSubview(smallCircle)
+        
+    }
+    
+    @objc func largeCircleTapped(){
+        print("large circle tapped")
+        self.largeCircle.removeFromSuperview()
+        self.smallCircle.removeFromSuperview()
+        self.largeCircleTapCounter += 1
+        self.newTarget()
+    }
+    
+    @objc func smallCircleTapped(){
+        print("small circle tapped")
+        self.largeCircle.removeFromSuperview()
+        self.smallCircle.removeFromSuperview()
+        self.smallCircleTapCounter += 1
+        self.newTarget()
+    }
+    
+    func endGame(){
+        self.playing = false
+        countdown.invalidate()
+        self.largeCircle.isUserInteractionEnabled = false
+        self.smallCircle.isUserInteractionEnabled = false
+    }
+    
+    @objc func countDown(){ //this function updates the countdown every 1 seconds
+        self.countDownCount = self.countDownCount - 1
+        
+        //if the countdown is 0 - the game is over
+        if(self.countDownCount < 0){
+            self.countDownLabel.text = "0"
+            self.endGame()
+            return
+        }
+        self.countDownLabel.text = String(format: "%d", self.countDownCount)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if(self.playing){
+            print("user tapped")
+            tapCounter += 1
+        }
+    }
+    
+}
+
